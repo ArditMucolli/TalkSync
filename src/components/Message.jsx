3a82e7ef173b24b9af09aa484dde7831701e32ee
ArditMucolli <mucolliardit@gmail.com>
2024-01-04 20:01:55 +0100

    Deleting css file for message and replacing with tailwind classes

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
-import "./message.css";
 
 const Message = ({ message }) => {
   const { currentUser } = useContext(AuthContext);
@@ -12,13 +11,16 @@ const Message = ({ message }) => {
   useEffect(() => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   }, [message]);
+
   return (
     <div
-      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+      className={`flex gap-4 mb-20 ${
+        message.senderId === currentUser.uid ? "flex-row-reverse" : "flex-row"
+      }`}
     >
-      <div>
+      <div className="flex items-center">
         <img
-          className="w-12 h-12 rounded-full object-cover sm:w-8 sm:h-8"
+          className="w-12 h-12 ml-2 rounded-full object-cover sm:w-8 sm:h-8"
           src={
             message.senderId === currentUser.uid
               ? currentUser.photoURL
@@ -26,16 +28,17 @@ const Message = ({ message }) => {
           }
           alt=""
         />
-        <span className="text-gray-400">just now</span>
       </div>
-      <div className="">
-        <p
-          className={`mb-4 pt-3 pb-3 pr-5 pl-5 rounded-t-none rounded-tr-lg rounded-b-lg rounded-l-lg 
-          ${message.senderId === currentUser.uid ? "ml-auto w-1/2" : "w-1/2"}`}
-        >
+      <div
+        className={`bg-blue-500 text-white p-5 rounded-lg ${
+          message.senderId === currentUser.uid
+            ? "ml-auto w-1/2 rounded-bl-none"
+            : "w-1/2 rounded-br-none"
+        }`}
+      >
+        <p className="mb-4 pt-3 pb-3 rounded-t-none rounded-tr-lg rounded-b-lg rounded-l-lg">
           {message.text}
         </p>
-
         {message.img && <img src={message.img} alt="" />}
       </div>
     </div>
