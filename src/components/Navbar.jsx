eabf9f35c1d7038e345d5e94b3643aa8f69d6e20
ArditMucolli <mucolliardit@gmail.com>
2024-01-08 19:21:51 +0100

    feat: dark mode design for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
   const { currentUser } = useContext(AuthContext);
 
   return (
-    <div className="bg-slate-100 sm:h-16 flex flex-col sm:flex-row items-center justify-between px-4 dark:bg-slate-900">
+    <div className="bg-slate-100 sm:h-16 flex flex-col sm:flex-row items-center justify-between px-4 dark:bg-slate-900 ">
       <div className="flex items-center gap-2">
         <img
           className="w-12 h-12 rounded-full object-cover sm:w-8 sm:h-8"
           src={currentUser.photoURL}
           alt=""
         />
-        <span className="text-black font-bold sm:text-base">
+        <span className="text-black dark:text-white font-bold sm:text-base">
           {currentUser.displayName}
         </span>
       </div>
@@ -22,7 +22,7 @@ const Navbar = () => {
         onClick={() => {
           signOut(auth);
         }}
-        className="bg-slate-50 py-1 px-2 rounded-md shadow-md hover:bg-gray-200 focus:outline-none mt-2 sm:mt-0"
+        className="bg-slate-50 dark:bg-slate-700 dark:text-white py-1 px-3 rounded-md shadow-md hover:bg-gray-200 focus:outline-none mt-2 sm:mt-0"
       >
         Logout
       </button>
